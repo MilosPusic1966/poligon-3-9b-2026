fb5f8c0f90e1062bd5f6a25859a2e3f1ac131cd4
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-03-16 12:09:49 +0100

    v0

---
+++ Tacka.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon_3_9b_2026
+{
+    internal class Tacka
+    {
+        // ovo sam upravo dodao. Ja.
+    }
+}
