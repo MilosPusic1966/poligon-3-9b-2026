d1b4e1ee89e0edf070eed4369a8b18fd24e072c9
milos pusic <milos.pusic@prvabeogim.edu.rs>
2026-03-23 18:45:08 +0100

    poligon

---
+++ Poligon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poligon_3_9b_2026
+{
+    internal class Poligon
+    {
+        public int br_temena;
+        public Tacka[] teme;
+        public Poligon(int n)
+        {
+            br_temena = n;
+            teme = new Tacka[n];
+        }
+        public static Poligon unos() 
+        {
+            return null;
+        }
+        public void stampa()
+        {
+
+        }
+        public void snimi()
+        {
+
+        }
+        public static Poligon ucitaj()
+        {
+            return null;
+        }
+    }
+}
